508e174ed16b2b21e32e4cb0bfaff65a561f4759
Velnene <111430674+Velnene@users.noreply.github.com>
2024-09-10 13:22:13 +0700

    fix: moved the function to a separate file

--- src/pages/GameBoard/GameBoard.tsx
+++ src/pages/GameBoard/GameBoard.tsx
@@ -1,83 +1,83 @@
 import { useGameStore } from '../../store/game.store'
+import { PlayerInfo } from './PlayerInfo'
+import { HandCard } from './HandCard'
+import { getSstyleRotation } from './hand-card/get-style-rotation'
 
 export function GameBoard() {
 	const { player, opponent, playCard } = useGameStore()
 
-	const calculateRotationOpponents = (index: number, total: number) => {
-		const middle = (total - 1) / 2
-		return -(index - middle) * 10
-	}
-
-	const calculateRotationPlayer = (index: number, total: number) => {
-		const middle = (total - 1) / 2
-		return (index - middle) * 10
-	}
-
 	return (
 		<>
-			<h1>Hi</h1>{' '}
-			<div>
-				<div>
-					<h2>Opponent</h2>
-					<p>HP: {opponent.health}</p>
-					<p>Mana: {opponent.mana}</p>
-				</div>
-				<div className='flex justify-center -mt-44'>
+			<div className='relative h-screen w-full grid grid-rows-3'>
+				<section>
+					<PlayerInfo player={opponent} typePlayer='opponent' />
+					<div className='flex justify-center'>
+						{opponent.deck
+							.filter((card) => !card.isOnBoard)
+							.slice(0, 5)
+							.map((card, index, arr) => (
+								<HandCard
+									card={card}
+									arrayLength={arr.length}
+									index={index}
+									key={card.id}
+									onClick={() => {
+										console.log(card)
+										playCard(card.id)
+									}}
+									isHided
+								/>
+							))}
+					</div>
+				</section>
+
+				<section className='flex flex-col gap-y-10'>
 					{opponent.deck
-						.filter((card) => !card.isOnBoard)
-						.slice(0, 5)
-						.map((card, index, arr) => (
+						.filter((card) => card.isOnBoard)
+						.map((card) => (
 							<button
-								className='h-60 w-40 bg-yellow-300 inline-block shadow -ml-4 rounded-lg'
-								style={{
-									transform: `rotate(${calculateRotationOpponents(index, arr.length)}deg)`,
-								}}
+								className='h-20 w-20 bg-yellow-300 inline-block shadow mx-1 rounded-lg'
 								key={card.id}
-								onClick={() => {
-									console.log(card)
-									playCard(card.id)
-								}}
 							>
 								<img
 									alt={card.name}
 									src={require('../../../public/assets/cards/exemple.jpg')}
+									draggable='false'
 								/>
-								text2
 							</button>
 						))}
-				</div>
-			</div>
-			<section>
-				{opponent.deck
-					.filter((card) => card.isOnBoard)
-					.map((card) => (
-						<button
-							className='h-40 w-60 bg-yellow-300 inline-block shadow mx-1 rounded-lg'
-							key={card.id}
-						>
-							<img
-								alt={card.name}
-								src={require('../../../public/assets/cards/exemple.jpg')}
-							/>
-						</button>
-					))}
-
-				<hr />
-				{player.deck
-					.filter((card) => card.isOnBoard)
-					.map((card) => (
-						<button
-							className='h-40 w-60 bg-yellow-300 inline-block shadow mx-1 rounded-lg'
-							key={card.id}
-						>
-							<img alt={card.name} src={card.imageUrl}></img>
-						</button>
-					))}
-			</section>
-			<div>
-				<h2>Player</h2>
-				<p>HP: {player.health}</p>
-				<p>Mana: {player.mana}</p>
+					{player.deck
+						.filter((card) => card.isOnBoard)
+						.map((card) => (
+							<button
+								className='h-20 w-20 bg-yellow-300 inline-block shadow mx-1 rounded-lg'
+								key={card.id}
+							>
+								<img alt={card.name} src={card.imageUrl}></img>
+							</button>
+						))}
+				</section>
+				<section>
+					<PlayerInfo player={player} typePlayer='player' />
+					<div className='bottom-10 absolute w-full'>
+						<div className='flex relative justify-center mx-auto'>
+							{player.deck
+								.filter((card) => !card.isOnBoard)
+								.slice(0, 5)
+								.map((card, index, arr) => (
+									<HandCard
+										card={card}
+										arrayLength={arr.length}
+										index={index}
+										key={card.id}
+										onClick={() => {
+											playCard(card.id)
+										}}
+									/>
+								))}
+						</div>
+					</div>
+				</section>
 			</div>
 		</>
 	)
---
+++ src/pages/GameBoard/hand-card/get-style-rotation.tsx
@@ -0,0 +1,14 @@
+export const getSstyleRotation = (
+	index: number,
+	total: number,
+	isPlayer: boolean
+) => {
+	const middle = (total - 1) / 2
+	const rotate = (index - middle) * 10
+	const distanceFromMiddle = Math.abs(index - middle)
+	const translateY = Math.pow(distanceFromMiddle, 2) * 15
+	return {
+		rotate: isPlayer ? rotate : -rotate,
+		translateY: isPlayer ? translateY : -translateY,
+	}
+}
