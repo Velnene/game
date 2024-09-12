ea8e71744b7bb5491377eb67eb52866e4fd9d0dd
Velnene <111430674+Velnene@users.noreply.github.com>
2024-09-12 11:36:41 +0700

    feat: add Loader

---
+++ src/components/ui/loader/Loader.tsx
@@ -0,0 +1,5 @@
+import AtomicSpinner from 'atomic-spinner'
+
+export function MyLoader() {
+	return <AtomicSpinner />
+}
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,15 +1,24 @@
+import { useTransition } from 'react'
 import { Butoon } from '../../components/ui/button/Botton'
 import { useGameStore } from '../../store/game.store'
 import { AudioPlayer } from '../GameBoard/audio-player/AudioPlayer'
+import { MyLoader } from '../../components/ui/loader/Loader'
 
 export function Home() {
+	const [isPending, startTransition] = useTransition()
+
 	const { startGame } = useGameStore()
+	const onClickStartGame = () => {
+		startTransition(() => {
+			startGame()
+		})
+	}
 	return (
 		<div className='flex items-center flex-col justify-center gap-4 h-screen'>
 			<h1 className='text-7xl text-white font-bold'>Go Play Game </h1>
 			<div className='page text-blue-700'>
-				<Butoon variant='primary' onClick={startGame} isCircle>
-					Start
+				<Butoon variant='primary' onClick={onClickStartGame} isCircle>
+					{isPending ? <MyLoader /> : 'Start'}
 				</Butoon>
 				<AudioPlayer />
 				<div className='card'></div>
